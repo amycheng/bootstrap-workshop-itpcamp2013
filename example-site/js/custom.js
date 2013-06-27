4877d0ef0576f38716cb3d28759d994282aeef86
Amy Cheng <amy@amycheng.info>
2013-06-26 21:32:18 -0400

    the example site we built during the workshop

---
+++ example-site/js/custom.js
@@ -0,0 +1,13 @@
+/*
+   Custom Javascript
+   */
+
+( function( $ ) {
+
+
+    $(document).ready(function(){
+        console.log('init');
+        $('#cool-carousel').carousel();
+    });
+
+}( jQuery ) );
